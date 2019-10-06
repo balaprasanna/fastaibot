8fbba207df46703fc986a3ff2b47f8ec0d4091d3
balaprasanna <balaprasannav2009@gmail.com>
2019-10-06 16:58:54 +0800

    done

--- demoapp.py
+++ demoapp.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify, make_response
 from fastai.core import *
 from fastai.vision import *
 from fastai.metrics import error_rate
+import io
+from contextlib import redirect_stdout
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
@@ -16,7 +19,12 @@ def dummy():
     path = untar_data(URLs.MNIST_TINY)
     data = ImageDataBunch.from_folder(path)
     learn = cnn_learner(data, models.resnet18, metrics=accuracy)
-    learn.fit(1)
-    return "done..."
+    
+    f = io.StringIO()
+    with redirect_stdout(f):
+        learn.fit(1)
+    out = f.getvalue()
+    
+    return out
 
 app.run('0.0.0.0', port=9000)
